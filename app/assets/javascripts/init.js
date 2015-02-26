0397288e6e28ad4870b9f42eff4cb1c94ae6d5e0
vmakarenko <vladimir@makareko.io>
2015-02-26 23:27:43 +0300

    v0.1

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,113 +1,18 @@
-//
-//cellSize = window.height / hTotal,                                 // Размер одной ячейки на карте
-//    pic = new Image(),                        // "Создаём" изображение
-//    map =                                     // Карта уровня двумерным массивом
-//        [
-//            [
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ], // 1ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 1, y: 1},
-//                {x: 2, y: 1},
-//                {x: 3, y: 1},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ], // 2ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 1, y: 2},
-//                {x: 2, y: 2},
-//                {x: 3, y: 2},
-//                {x: 1, y: 4},
-//                {x: 1, y: 3},
-//                {x: 1, y: 3},
-//                {x: 1, y: 3}
-//            ], // 3ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 3, y: 4},
-//                {x: 2, y: 3},
-//                {x: 3, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 3},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ], // 4ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 3, y: 4},
-//                {x: 2, y: 4},
-//                {x: 3, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 3},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ], // 5ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 3},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 3},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ], // 6ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 3},
-//                {x: 1, y: 3},
-//                {x: 1, y: 3},
-//                {x: 1, y: 3},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ], // 7ый ряд
-//            [
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4},
-//                {x: 1, y: 4}
-//            ]  // 8ый ряд
-//        ]; // Первая и вторая координата (x и y соответственно) задают фрагмент в исходном изображении
-//// Размер холста равный 8х8 клеток
-//pic.onload = function () {  // Событие onLoad, ждём момента пока загрузится изображение
-//    for (var j = 0; j < 8; j++)
-//        for (var i = 0; i < 8; i++)
-//            // перебираем все значения массива 'карта' и в зависимости от координат вырисовываем нужный нам фрагмент
-//            ctx.drawImage(pic, (map[i][j].x - 1) * cellSize, (map[i][j].y - 1) * cellSize, 32, 32, j * cellSize, i * cellSize, 32, 32)
-//
-//
-//}
-
 /**
  * Created by VMakarenko on 2/23/15.
  */
 
 window.onload = function () {
-    const wTotal = 8;
-    const hTotal = 8;
-    var cellSize = 32;
+    const wTotal = 20;
+    const hTotal = 20;
+    var cellSize = 20;
+    var pause = 25;
     var picsTotal;
     var currentPic = {
         id: 0,
         h: undefined,
         w: undefined
-    }
+    };
 
     var picArray = _.range(hTotal).map(function () {
         return _.range(wTotal).map(function () {
@@ -128,6 +33,7 @@ window.onload = function () {
         }
         if (h >= hTotal) {
             h = 0;
+            currentPic.id++;
         }
     }
     $.get('http://localhost:3000/api/pic/pic_total',function(result){
@@ -147,7 +53,7 @@ window.onload = function () {
 
     }
 
-    var timer = setInterval(getNewImgData, 1000);
+    var timer = setInterval(getNewImgData, pause);
 
 
 }
--- app/controllers/api/pic_controller.rb
+++ app/controllers/api/pic_controller.rb
@@ -1,14 +1,15 @@
 class Api::PicController < ApplicationController
   def get_pic
-    @pic_id, @w, @h = params[:pic_id].to_i, params[:w].to_i,params[:h].to_i
+    @pic_id, @w, @h = params[:pic_id].to_i % Dir[Rails.root.join('app', 'assets','images','*.jpg')].length,\
+        params[:w].to_i,params[:h].to_i
     # get current image path
     @path = Dir[Rails.root.join('app', 'assets','images','*.jpg')][@pic_id]
     @imageW, @imageH = FastImage.size(@path)
-    @wSize = @imageW/8
-    @hSize = @imageH/8
+    @wSize = @imageW/20
+    @hSize = @imageH/20
 
-    @img = Image.read(@path).crop(@w*@wSize, @h*@hSize, @wSize, @hSize)
-    send_data @img, :filename => "1.jpg",
+    @img = Magick::Image.read(@path).first.crop(@w*@wSize, @h*@hSize, @wSize, @hSize)
+    send_data @img.to_blob, :filename => "1.jpg",
               :disposition => 'inline',
               :type => "image/jpeg"
   end
